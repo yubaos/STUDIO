78d35b1833a70709c29109af0fb6312e0bcc2218
yubaos <59527593+yubaos@users.noreply.github.com>
2026-07-29 12:00:32 +0800

    Update script.js

--- footprints/js/script.js
+++ footprints/js/script.js
@@ -31,7 +31,7 @@
     KualaLumpur:    'https://img.yubaos.com/Gallery/20161004kl.webp',
     xiamen:    'https://img.yubaos.com/Gallery/20140410xm.webp',
     kyoto:     'https://image.qwenlm.ai/public_source/ad1849f7-810e-4742-98d1-aa785709bea4/14dc9e83f-de7f-4aec-9997-d29e554a3cde.png',
-    fuji:     'https://image.qwenlm.ai/public_source/ad1849f7-810e-4742-98d1-aa785709bea4/14dc9e83f-de7f-4aec-9997-d29e554a3cde.png',
+    Langkawi:     'https://img.yubaos.com/video/Langkawi.jpg',
     shanghai: 'https://image.qwenlm.ai/public_source/ad1849f7-810e-4742-98d1-aa785709bea4/1ac501088-11d3-40be-8bc0-873924b0f1ba.png',
     bangkok:  'https://image.qwenlm.ai/public_source/ad1849f7-810e-4742-98d1-aa785709bea4/1ca9d2404-da75-4a51-9b7a-49446ac90d2e.png',
     hanoi:    'https://image.qwenlm.ai/public_source/ad1849f7-810e-4742-98d1-aa785709bea4/16cfa3b16-9280-4488-956e-5089d57c87f2.png',
